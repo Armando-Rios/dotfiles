27656a28b2811f7ce0b22ae2f08871e89425c86b
Armando Rios <armandorios.dev@proton.me>
2024-09-01 18:58:14 -0700

    powershell startup parameters

--- windows/powershell/Microsoft.Powershell_profile.ps1
+++ windows/powershell/Microsoft.Powershell_profile.ps1
@@ -1,1 +1,31 @@
 # powershell startup parameters
+
+# oh-my-posh prompt call
+oh-my-posh init pwsh --config ~/.dotifles/terminal/themes/default.opm.json | Invoke-Expression
+
+# Terminal icons import
+Import-Module Terminal-Icons
+
+# Aliases
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias grep findstr
+Set-Alias tig "C:\Program Files\Git\usr\bin\tig.exe"
+Set-Alias less "C:\Program Files\Git\usr\bin\less.exe"
+
+function dev {
+    param (
+        [string]$project = ""
+    )
+    $workDir = "D:\Dev"
+    if($project -ne "") {
+        $targetDir = Join-Path $workDir $project
+    } else {
+        $targetDir = $workDir
+    }
+    if(Test-Path $targetDir) {
+        Set-Location $targetDir
+    } else {
+        Write-Host "El proyecto '$project' no existe en '$workDir'" - ForegroundColor Red
+    }
+}
