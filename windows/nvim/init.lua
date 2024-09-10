805925f7c2d0b58bf3007e1b6bf7a963e75f1389
Armando Rios <armandorios.dev@proton.me>
2024-09-10 05:45:51 +0000

    refactor path files

--- windows/nvim/init.lua
+++
@@ -1,4 +0,0 @@
--- Neovim main configuration in Lua.
--- General options, plugins and key mappings are loaded here.
-require("config.settings")
-require("config.keymaps")
--- windows/nvim/lua/config/settings.lua
+++
@@ -1,7 +0,0 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.fillchars = { eob = " " }
