5d51dd2066e73f012323d7ed64eff28e178a6455
Armando Rios <armandorios.dev@proton.me>
2024-10-04 22:08:40 -0700

    add keymaps descriptions.

--- common/nvim/lua/config/keymaps.lua
+++ common/nvim/lua/config/keymaps.lua
@@ -2,22 +2,22 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- keymap for write
-keymap("n", "<leader>w", ":w<CR>", opts)
+keymap("n", "<leader>w", ":w<CR>", { noremap = true, silent = true, desc = "Write File" })
 
 -- keymap for quit
-keymap("n", "<leader>q", ":bd<CR>", opts)
+keymap("n", "<leader>q", ":bd<CR>", { noremap = true, silent = true, desc = "Close Buffer" })
 
 -- keymap for exit nvim
-keymap("n", "<leader>Q", ":qa!<CR>", opts)
+keymap("n", "<leader>Q", ":qa!<CR>", { noremap = true, silent = true, desc = "Exit Nvim"})
 
 -- keymap select option
-keymap("n", "<C-a>", "gg<S-v>G") -- select all
+keymap("n", "<C-a>", "gg<S-v>G", { desc = "Select all"}) -- select all
 
 -- nvim-tree
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "File Explorer"})
 
 -- telescope
 
 local telescope = require('telescope.builtin')
-keymap("n", "<leader>fg", telescope.live_grep, opts)
-keymap("n", "<leader>p", telescope.find_files, opts)
+keymap("n", "<leader>fg", telescope.live_grep, { noremap = true, silent = true, desc = "Live Grep"})
+keymap("n", "<leader>p", telescope.find_files, {noremap= true, silent = true, desc = "Find Files"})
