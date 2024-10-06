99d1e04d50b3b17227768979978997efbf5d82dd
Armando Rios <armandorios.dev@proton.me>
2024-10-05 18:09:47 -0700

    nvim noice plugin added.

---
+++ common/nvim/lua/plugins/noise.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "rcarriga/nvim-notify",
+        config = function()
+            vim.notify = require("notify")
+            require("notify").setup({
+                max_width = 200,
+                fps = 30,
+                timeout = 1000,
+                stages = "static",
+                top_down = false,
+            })
+        end,
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {},
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+    },
+}
