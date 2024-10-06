51276040febd2152e53c90c08491a52c624cbcaa
Armando Rios <armandorios.dev@proton.me>
2024-10-05 18:08:09 -0700

    oil file-explorer plugin add.

--- common/nvim/lua/plugins/file-explorer.lua
+++ common/nvim/lua/plugins/file-explorer.lua
@@ -1,26 +1,32 @@
-return { 
-    "nvim-tree/nvim-tree.lua",
-    proiority = 1000,
-    config = function ()
-        require("nvim-tree").setup({
-            sort = {
-                sorter = "case_sensitive",
-            },
-            view = {
-                width = 30,
-                side = 'right',
-            },
-            renderer = {
-                group_empty = true,
-            },
-            filters = {
-                dotfiles = true,
-            },
-            actions = {
-                open_file = {
-                    quit_on_open = true
-                }
-            },
-        })  
-    end
+return {
+	{
+		"nvim-tree/nvim-tree.lua",
+		proiority = 1000,
+		config = function()
+			require("nvim-tree").setup({
+				sort = {
+					sorter = "case_sensitive",
+				},
+				view = {
+					width = 30,
+					side = "right",
+				},
+				renderer = {
+					group_empty = true,
+				},
+				filters = {
+					dotfiles = true,
+				},
+				actions = {
+					open_file = {
+						quit_on_open = true,
+					},
+				},
+			})
+		end,
+	},
+    {
+        'stevearc/oil.nvim',
+        opts = {}
+    }
 }
